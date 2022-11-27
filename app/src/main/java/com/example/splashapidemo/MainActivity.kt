fc396450d2b105c33d505e9dcc0c69b98ddca503
Yamini <yamini@chetnainfotech.com>
2022-11-27 21:46:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashapidemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.splashapidemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
